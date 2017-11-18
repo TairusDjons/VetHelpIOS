d1d3674c3ae4dc8d70777a05e8f332dbd21d0778
VetDev1 <vetdev1@mac-mini-administrator.local>
2017-11-18 14:39:07 -0800

    Add OAuth SingleTon

---
+++ VetHelpIOS/API/Oauth.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  VetHelpIOS
+//
+//  Created by VetDev1 on 18.11.17.
+//  Copyright © 2017 VetDev1. All rights reserved.
+//
+
+import Foundation
+import p2_OAuth2
+class OAuth{
+    static var oauth2:OAuth2PasswordGrant?
+
+}
