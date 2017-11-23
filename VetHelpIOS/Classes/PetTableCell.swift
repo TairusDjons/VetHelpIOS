53e9bd52f861a0f043a023fd958ee4e852684b9b
VetDev1 <vetdev1@mac-mini-administrator.local>
2017-11-23 04:03:59 -0800

    Add controllers for Pet Table and Pet Cell

---
+++ VetHelpIOS/Classes/PetTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  PetTableCell.swift
+//  VetHelpIOS
+//
+//  Created by VetDev1 on 21.11.17.
+//  Copyright © 2017 VetDev1. All rights reserved.
+//
+
+import UIKit
+
+class PetTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
