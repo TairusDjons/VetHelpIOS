12ede52ffec06ee8c3fa83bd5d79c9f26090487e
VetDev1 <vetdev1@miniadmistrator.vrn.dataart.net>
2018-02-09 05:29:48 -0800

    added MyProfileController

---
+++ VetHelpIOS/Controllers/MyProfileController.swift
@@ -0,0 +1,38 @@
+//
+//  MyProfileController.swift
+//  VetHelpIOS
+//
+//  Created by VetDev1 on 09.02.2018.
+//  Copyright © 2018 VetDev1. All rights reserved.
+//
+
+import UIKit
+
+class MyProfileController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func logoutOnTap(_ sender: Any) {
+        ClientService.logout()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
