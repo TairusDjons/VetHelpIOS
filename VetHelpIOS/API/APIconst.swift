356b2d9a3d18ffee3cdc1f9e225dc30cbb840714
VetDev1 <vetdev1@miniadmistrator.vrn.dataart.net>
2018-02-09 05:20:40 -0800

    added API constants

---
+++ VetHelpIOS/API/APIconst.swift
@@ -0,0 +1,16 @@
+//
+//  APIconst.swift
+//  VetHelpIOS
+//
+//  Created by VetDev1 on 21.01.2018.
+//  Copyright © 2018 VetDev1. All rights reserved.
+//
+
+import Foundation
+public class API {
+    static let IsEmailAvailable = "https://vethelp.azurewebsites.net/api/Account/IsEmailAvailable/"
+    static let IsUsernameAvailable = "https://vethelp.azurewebsites.net/api/Account/IsUsernameAvailable/"
+    static let ForgotPassword = "https://vethelp.azurewebsites.net/api/Account/ForgotPassword"
+    static let Register = "https://vethelp.azurewebsites.net/api/Account/Register"
+    static let Login = "https://vethelp.azurewebsites.net/connect/token"
+}
