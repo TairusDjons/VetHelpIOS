f98b8bb590cc339fed02cf3b983346ff67a43853
VetDev1 <vetdev1@mac-mini-administrator.local>
2018-02-09 05:26:19 -0800

    added pet classes

---
+++ VetHelpIOS/Classes/Pet.swift
@@ -0,0 +1,20 @@
+//
+//  Pet.swift
+//  VetHelpIOS
+//
+//  Created by VetDev1 on 24.01.2018.
+//  Copyright © 2018 VetDev1. All rights reserved.
+//
+
+import Foundation
+public class Pet {
+    var name = "Unnamed"
+    var type = "Untyped"
+    var breed = "Unbreeded"
+    var sex = "None"
+    var birthdate = Date()
+    
+    init() {
+        
+    }
+}
---
+++ VetHelpIOS/Classes/TempPet.swift
@@ -0,0 +1,12 @@
+//
+//  TempPet.swift
+//  VetHelpIOS
+//
+//  Created by VetDev1 on 24.01.2018.
+//  Copyright © 2018 VetDev1. All rights reserved.
+//
+
+import Foundation
+class TempPet {
+    let pet = Pet()
+}
